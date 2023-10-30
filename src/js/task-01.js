eda7557231f25dcd7534f98d7cb7e5d0031aa2c6
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2023-10-30 22:52:12 +0200

    task - 01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,17 @@
-console.log(document)
-const elm = document.querySelector("ul")
-console.log(elm);
+'use strict'
+const elementItem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${elementItem.length}`);
+
+
+
+const categoriesArrey = document.querySelectorAll('.item');
+categoriesArrey.forEach(nameCategories => {
+    const nameCategoriesArrey = nameCategories.querySelectorAll(`h2`);
+    console.log(`Category: ${nameCategoriesArrey[0].textContent}`);
+    
+    const arrayCategoriesObject = nameCategories.querySelectorAll(`ul li`);
+    const lengthElementCategoriesObject = arrayCategoriesObject.length;
+    console.log(`Elements: ${lengthElementCategoriesObject}`);
+    
+    
+});
