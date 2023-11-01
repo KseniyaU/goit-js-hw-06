8616a9b3a12deb606450862527993a639676dd9e
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2023-11-01 22:33:45 +0200

    task - 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const id = document.querySelector("#ingredients")
+id.classList.add("item")
+function createMarkup(item) {
+  const markup = item.map(ingredient => { 
+    const li = document.createElement("li")
+    li.textContent = ingredient;
+    return li;
+
+  })
+  id.append(...markup);
+}
+createMarkup(ingredients);
+
+
+
+//варіант 2
+
+// function createMarkup(ingredients) {
+//   const id = document.querySelector("#ingredients")
+  
+//   const markup = ingredients.map(ingredient => { 
+//     const li = document.createElement("li")
+//     li.append(ingredient)
+//     return li;
+//   })
+//   id.append(...markup);
+// }
+// createMarkup(ingredients);
